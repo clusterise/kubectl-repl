285e6ace09be94351c1bccc03d03bacb17e5ed87
Mikuláš Dítě <rullaf@gmail.com>
2017-10-16 12:36:56 +0200

    refactor shell invocation

--- main/kubectl.go
+++ main/kubectl.go
@@ -7,13 +7,16 @@ import (
 	"strings"
 )
 
-func kubectlSh(cmd string) (string, error) {
-	shell := fmt.Sprintf("kubectl -n %s %s", namespace, cmd)
-
+func sh(shell string) (string, error) {
 	if verbose {
 		color.Yellow(fmt.Sprintf("+ %s\n", shell))
 	}
 	process := exec.Command("/bin/sh", "-c", shell)
 	bytes, err := process.CombinedOutput()
 	return strings.TrimRight(string(bytes), "\n"), err
+}
+
+func kubectlSh(cmd string) (string, error) {
+	shell := fmt.Sprintf("kubectl -n %s %s", namespace, cmd)
+	return sh(shell)
 }
