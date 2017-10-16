0d68360e7bb568360ef11e4ef29ca79b65b17e11
Mikuláš Dítě <rullaf@gmail.com>
2017-10-16 14:25:52 +0200

    fix release script invalid version arg usage

--- release.sh
+++ release.sh
@@ -3,14 +3,15 @@ set -euo pipefail
 IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-VERSION="$3"
+VERSION="$1"
 
 function build {
-    GOOS="$1" GOARCH="$2" go build -o "releases/kubectl-repl-$1-$2-version" main/*.go
+    GOOS="$1" GOARCH="$2" go build -o "releases/kubectl-repl-$1-$2-$VERSION" main/*.go
 }
 
 mkdir -p "$DIR/releases"
 
+set -v
 build windows 386
 build windows amd64
 build darwin 386
