9e85e2d52397a51d125969308fef3ee79051752a
Mikuláš Dítě <dite@mangoweb.cz>
2018-07-08 10:19:21 +0200

    add indexed vars
    All columns in output can now be accessed with $var:index syntax.
    For simplicy (and backwards compatibility) $var defaults to $var:1.

--- main/variables.go
+++ main/variables.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"regexp"
+	"strconv"
+	"fmt"
 )
 
 var (
@@ -9,17 +11,38 @@ var (
 	pattern   *regexp.Regexp
 )
 
+const (
+	groupVar = 1
+	groupColumn = 3
+)
+
 func substituteForVars(text string) (string, error) {
 	if pattern == nil {
-		pattern = regexp.MustCompile(`[$]\w+\b`)
+		pattern = regexp.MustCompile(`[$](\w+)(:(\d+))?\b`)
 	}
 
-	var err error
+	var outerError error
 	output := pattern.ReplaceAllStringFunc(text, func(match string) string {
-		if val, ok := variables[match[1:]]; ok {
-			return val[0]
+		parts := pattern.FindStringSubmatch(match)
+		varName := parts[groupVar]
+		if val, ok := variables[varName]; ok {
+			col := parts[groupColumn]
+			if col == "" {
+				return val[0]
+			}
+			colInt, err := strconv.ParseInt(col, 10, 64)
+			if err != nil {
+				outerError = fmt.Errorf("could not parse %v as column index", match)
+				return "(ERROR)"
+			}
+			index := int(colInt - 1)
+			if index < 0 || index >= len(val) {
+				outerError = fmt.Errorf("variable $%v has no value in column %v", varName, col)
+				return "(ERROR)"
+			}
+			return val[index]
 		}
 		return match
 	})
-	return output, err
+	return output, outerError
 }
--- main/variables_test.go
+++ main/variables_test.go
@@ -20,4 +20,13 @@ func TestSubstitution(t *testing.T) {
 	assertSubstituion(t, "alphaalphaalpha", "$a$a$a")
 	assertSubstituion(t, "triple", "$aaa")
 }
+
+func TestSubstitutionIndex(t *testing.T) {
+	variables = map[string][]string{
+		"a":   {"alpha", "alpha2", "alpha3"},
+		"aaa": {"triple"},
+	}
+	assertSubstituion(t, "pre alpha post", "pre $a:1 post")
+	assertSubstituion(t, "alphaalpha2alpha3", "$a:1$a:2$a:3")
+	assertSubstituion(t, "triple", "$aaa")
 }
